5aa17b5573b8e01688a9085d581d0e47d34eaf29
Jaagup Kippar <jaagup@tlu.ee>
2022-02-07 14:35:24 +0200

    Create Funktsioonid1.java

---
+++ 01_funktsioon/Funktsioonid1.java
@@ -0,0 +1,16 @@
+public class Funktsioonid1{
+	public static double sentimeetriteks(double tollid){
+		return tollid*2.54;
+	}
+	public static double tollideks(double sentimeetrid){
+		return sentimeetrid/2.54;
+	}
+	//Tehke funktsiooniga sentimeetrid tollideks
+	public static void main(String[] args){
+		System.out.println(tollideks(10));
+		if(args.length>0){
+			double t=Double.parseDouble(args[0]);
+			System.out.println(sentimeetriteks(t));
+		}
+	}
+}
