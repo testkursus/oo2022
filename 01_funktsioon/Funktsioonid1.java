cb8e345158c6c492abfeb4c65a66fa794fb32342
Jaagup Kippar <jaagup@tlu.ee>
2022-02-07 14:42:43 +0200

    Update Funktsioonid1.java

--- 01_funktsioon/Funktsioonid1.java
+++ 01_funktsioon/Funktsioonid1.java
@@ -8,6 +8,7 @@ public class Funktsioonid1{
 	//Tehke funktsiooniga sentimeetrid tollideks
 	public static void main(String[] args){
 		System.out.println(tollideks(10));
+		System.out.println(tollideks(10));
 		if(args.length>0){
 			double t=Double.parseDouble(args[0]);
 			System.out.println(sentimeetriteks(t));
